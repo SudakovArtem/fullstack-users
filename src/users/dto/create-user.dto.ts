36f029a3f527a418b3e373469ea971d0e22d08ed
Artem Sudakov <a.sudakov@inetstudio.ru>
2026-09-22 18:29:54 +0300

    fix: fix review

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,7 +3,7 @@ import { Transform } from 'class-transformer';
 
 export class CreateUserDto {
   @IsString({ message: 'Name must be a string' })
-  @Transform(({ value }) => value?.trim())
+  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   @IsNotEmpty({ message: 'Name must not be empty' })
   name: string;
 }
